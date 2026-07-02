ff8ac049065845cc3fca4e2f1cd46ce12f91c06e
Federico Cruz <federico.cruz.lozano@hotmail.com>
2026-07-02 14:54:41 -0600

    chore: Change page title
    Change the page title from Nimara Storefront to Joyeria Proyecto 705

--- apps/storefront/src/envs/client.ts
+++ apps/storefront/src/envs/client.ts
@@ -41,7 +41,7 @@ const schema = z.object({
   ),
   NEXT_PUBLIC_DEFAULT_PAGE_TITLE: z.preprocess(
     emptyStringToUndefined,
-    z.string().default("Nimara Storefront"),
+    z.string().default("Joyeria Proyecto 705"),
   ),
   NEXT_PUBLIC_SALEOR_API_URL: z.preprocess(
     emptyStringToUndefined,
